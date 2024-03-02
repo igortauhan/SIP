c9d25e307c4ddcace8e6e563bae3ec6b283e2684
Igor Tauhan <igortauhan.ds@gmail.com>
2024-03-02 20:26:40 -0300

    WIP

--- service/system_commands.py
+++ service/system_commands.py
@@ -2,22 +2,22 @@ import os
 
 
 def list_files(directory):
-    return os.listdir()
+    return os.listdir(directory)
 
 
-def convert_pdf_to_text(file):
+def convert_pdf_to_text(directory, file):
     try:
-        os.system(f'pdftotext -layout {file}')
+        os.system(f'pdftotext -layout {directory}/{file}')
     except Exception as e:
         print(e)
 
 
-def search_text(file, text):
+def search_text(directory, file, text):
     try:
-        return os.popen(f'cat {file} | grep -i {text}').read()
+        return os.popen(f'cat {directory}/{file} | grep -i {text}').read()
     except Exception as e:
         print(e)
 
 
-def delete_file(file):
-    os.remove(file)
+def delete_file(directory, file):
+    os.remove(f'{directory}/{file}')
